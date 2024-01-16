1c401eacef3695878635e335ac5b4c68ec07f3e5
Cleiton Araujo <cleitonaraujo1311@gmail.com>
2024-01-15 22:42:17 -0300

    Correções

--- projeto_ecomerce/settings.py
+++ projeto_ecomerce/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dn$bu4g$h-3(!w7*hoed3jm-n%9ys1udkb-ka(a5u92d0ft9)0'
 SECRET_KEY = os.environ.get('SECRET_KEY', default=SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ['*']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
